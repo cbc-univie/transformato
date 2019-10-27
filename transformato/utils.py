6e977d831ec354f1b7493fb0fb3f123e6a512b9d
wiederm <marcus.wieder@univie.ac.at>
2019-10-27 02:51:38 +0200

    small fixes

--- transformato/utils.py
+++ transformato/utils.py
@@ -10,7 +10,7 @@ def get_toppar_dir():
     """Returns the bin directory of this package"""
     return os.path.abspath(os.path.join(os.path.dirname(__file__), 'toppar'))
 
-def load_config_yaml(config, data_dir, output_dir):
+def load_config_yaml(config, input_dir, output_dir):
 
     with open(f"{config}", 'r') as stream:
         try:
@@ -21,7 +21,7 @@ def load_config_yaml(config, data_dir, output_dir):
     # set the bin, data and analysis dir
     settingsMap['bin_dir'] = get_bin_dir()
     settingsMap['analysis_dir_base'] = f"{output_dir}"
-    settingsMap['data_dir_base'] = f"{data_dir}"
+    settingsMap['data_dir_base'] = f"{input_dir}"
     system_name = f"{settingsMap['system']['structure1']['name']}-{settingsMap['system']['structure2']['name']}"
     settingsMap['system_dir'] = f"{settingsMap['analysis_dir_base']}/{system_name}"
     settingsMap['cluster_dir'] = f"/data/local/{system_name}"
