9077d12596c7b38201ea37ef22c937b076bb81b8
wiederm <marcus.wieder@univie.ac.at>
2019-10-30 14:58:09 +0100

    split atom charge transformation from bonded mutation object

--- transformato/__init__.py
+++ transformato/__init__.py
@@ -16,3 +16,12 @@ versions = get_versions()
 __version__ = versions['version']
 __git_revision__ = versions['full-revisionid']
 del get_versions, versions
+
+ 
+import logging
+# format logging message
+FORMAT = "[%(filename)s:%(lineno)s - %(funcName)1s()] %(message)s"
+# set logging level
+logging.basicConfig(format=FORMAT,
+    datefmt='%d-%m-%Y:%H:%M',
+    level=logging.INFO)
