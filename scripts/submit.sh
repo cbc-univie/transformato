2c7faa9d86eba211ad2702773ea0e50fa555f72c
wiederm <marcus.wieder@univie.ac.at>
2019-11-29 18:37:05 +0100

    adding documentation

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
+# beware: this script is written to be used with the sge gird engine
+# if you are running this on your local computer or on a different 
+# grid engine the call on line 25 should be changed to `bash` 
+# or something else
 
-config_file=$1
-working_dir=$2
-structure=$3
+config_file=$1 # path to the yaml file
+output_dir=$2 # where to write the output json files
+structure=$3 # the name of the system as provided in the yaml flie
 
 hostname
 echo ${config_file}
-echo ${working_dir}
+echo ${output_dir}
 echo ${structure}
 
 
-nr_of_states=$(find ${working_dir}/${structure} -mindepth 1 -maxdepth 1 -type d | wc -l)
+nr_of_states=$(find ${output_dir}/${structure} -mindepth 1 -maxdepth 1 -type d | wc -l)
 
 for i in $(seq 1 1 ${nr_of_states})
     do 
@@ -18,6 +22,6 @@ for i in $(seq 1 1 ${nr_of_states})
         do 
         echo $i
         echo $j 
-        qsub calculate_energies.sh ${config_file} ${working_dir} ${structure} ${i} ${j}
+        qsub calculate_energies.sh ${config_file} ${output_dir} ${structure} ${i} ${j}
     done
 done
