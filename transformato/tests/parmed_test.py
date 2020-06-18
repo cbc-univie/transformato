3d8dd3dc31065e7a0149db358ac97164cbad510e
bbraunsfeld <b.braunsfeld@gmail.com>
2020-06-18 17:19:30 +0200

    expansion of the parmed test

--- transformato/tests/parmed_test.py
+++ transformato/tests/parmed_test.py
@@ -24,3 +24,20 @@ print (params_non_std.angle_types[('HGA3' , 'CG3311' , 'HGA3')])
 
 print (params_non_std.dihedral_types[('HGA3' , 'CG331' , 'CG3311' ,'HGA3')])
 print (params_non_std.dihedral_types[('HGA3' , 'CG3311' , 'CG331' ,'HGA3')])
+
+print ('non std2')
+params_non_std2 = CharmmParameterSet('/home/master/debug/non_std2.prm', '/home/master/debug/non_std2.rtf')
+print (params_non_std2.bond_types[('CG3312' , 'CG3311')])
+print (params_non_std2.bond_types[('CG3312' , 'HGA3')])
+print (params_non_std2.bond_types[('CG3311' , 'HGA3')])
+
+print (params_non_std2.angle_types[('CG3311' , 'CG3312' , 'HGA3')])
+print (params_non_std2.angle_types[('CG3312' , 'CG3311' , 'HGA3')])
+print (params_non_std2.angle_types[('HGA3' , 'CG3312' , 'HGA3')])
+print (params_non_std2.angle_types[('HGA3' , 'CG3311' , 'HGA3')])
+
+print (params_non_std2.dihedral_types[('HGA3' , 'CG3312' , 'CG3311' ,'HGA3')])
+print (params_non_std2.dihedral_types[('HGA3' , 'CG3311' , 'CG3312' ,'HGA3')])
+
+assert(id(params_non_std.dihedral_types[('HGA3' , 'CG331' , 'CG3311' ,'HGA3')]) == id(params_non_std.dihedral_types[('HGA3' , 'CG3311' , 'CG331' ,'HGA3')])) == True
+assert(id(params_non_std2.dihedral_types[('HGA3' , 'CG3312' , 'CG3311' ,'HGA3')]) == id(params_non_std2.dihedral_types[('HGA3' , 'CG3311' , 'CG3312' ,'HGA3')])) == True
