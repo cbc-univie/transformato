7a93af0cb62b0f0eb7a8fdaa77a8c258693aae32
bbraunsfeld <b.braunsfeld@gmail.com>
2020-10-15 13:36:54 +0200

    rst file enabled for vacuum_run

--- transformato/charmm_factory.py
+++ transformato/charmm_factory.py
@@ -183,7 +183,7 @@ open write unit 21 file name charmm_gasp.dcd
  
 DYNA lang leap start time 0.001 nstep @nstep -
     nprint {nstout} iprfrq {round(nstep/20)} -
-    iunread -1 iunwri -1 iuncrd 21 iunvel -1 kunit -1 -
+    iunread -1 iunwri 12 iuncrd 21 iunvel -1 kunit -1 -
     nsavc {nstdcd} nsavv 0 -
     rbuf 0. tbath @temp ilbfrq 0  firstt @temp -
     echeck 0
