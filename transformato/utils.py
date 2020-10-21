1d637bbcfe49532580e71feddeb96990fb2cefd5
bbraunsfeld <b.braunsfeld@gmail.com>
2020-10-21 13:56:40 +0200

    psf_correction

--- transformato/utils.py
+++ transformato/utils.py
@@ -3,6 +3,8 @@ import os
 import parmed as pm
 import yaml
 
+from io import StringIO
+import io 
 
 def get_bin_dir():
     """Returns the bin directory of this package"""
@@ -78,3 +80,41 @@ def load_config_yaml(config, input_dir, output_dir):
     ] = f"{settingsMap['data_dir_base']}/{settingsMap['system']['structure2']['name']}/"
     settingsMap["system"]["name"] = system_name
     return settingsMap
+
+def psf_correction(StringIo_object):
+    """Correcting the issue with 2 missing spaces in the waterbox psf files"""
+    file = StringIo_object.readlines()
+    new_file = f"" 
+    flag = False
+    for line in file:
+        if flag == False:
+            new_file = f"{new_file}{line}"
+            flag = "!NATOM" in line
+        else:
+            if "!NBOND" in line:
+                new_file = f"{new_file}{line}"
+                flag = False
+            else:
+                line = line.split()
+                if len(line) != 11:
+                    new_file = f"{new_file}\n"
+                else:
+                    space = " "
+                    a = 10 - len(line[0])
+                    b = 1
+                    c = 9 - len(line[1]) 
+                    d = 9 - len(line[2])
+                    e = 9 - len(line[3])
+                    f = 9 - len(line[4])
+                    g = 17 - len(line[6]) - len(line[5])
+                    h = 14 - len(line[7])
+                    i = 12 - len(line[8])
+                    j = 10 - len(line[9])
+                    k = 18 - len(line[10])
+                    new_line = f"{space*a}{line[0]}{space*b}{line[1]}{space*c}{line[2]}{space*d}{line[3]}{space*e}{line[4]}{space*f}{line[5]}{space*g}{line[6]}{space*h}{line[7]}{space*i}{line[8]}{space*j}{line[9]}{space*k}{line[10]}\n"
+                    if len(new_line) == 119:
+                        new_file = f"{new_file}{new_line}"
+                    else:
+                        print("Error")
+                        
+    return new_file
