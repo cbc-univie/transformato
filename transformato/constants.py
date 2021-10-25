9abc3e81cf8cbf1d0c2c7b798b3963fbb56f22d2
wiederm <marcus.wieder@univie.ac.at>
2021-10-25 10:37:50 +0200

    move the testsystem_dir to constants

--- transformato/constants.py
+++ transformato/constants.py
@@ -3,15 +3,16 @@ from simtk import unit
 
 logger = logging.getLogger(__name__)
 
-default_platform = "GPU"  # CPU or GPU
+default_platform = "CPU"  # CPU or GPU
 temperature = 303.15 * unit.kelvin
 kB = unit.BOLTZMANN_CONSTANT_kB * unit.AVOGADRO_CONSTANT_NA
 kT = kB * temperature
-charmm_gpu = ''
-#charmm_gpu = 'domdec-gpu' #uncomment this if you want to use domdec-gpu
+charmm_gpu = ""
+# charmm_gpu = 'domdec-gpu' #uncomment this if you want to use domdec-gpu
 this = sys.modules[__name__]
 # we can explicitly make assignments on it
 this.NUM_PROC = 1
+loeffler_testsystems_dir = "/home/mwieder/Work/Projects/transformato-systems/"
 
 
 def initialize_NUM_PROC(n_proc):
@@ -37,8 +38,9 @@ def change_platform(configuration: dict):
     else:
         raise RuntimeError("something went wrong")
 
-def change_platform_to(configuration: dict, change_to:str):
-    
+
+def change_platform_to(configuration: dict, change_to: str):
+
     if change_to.upper() == "GPU":
         configuration["simulation"]["GPU"] = True
         print("Setting platform to GPU")
