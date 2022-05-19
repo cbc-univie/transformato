78ff3f7b8342688095efefe8b589e1549c95b18a
Alexander Grasser <a01526871@unet.univie.ac.at>
2022-05-19 15:59:53 +0200

    Restricted bonds to atoms in common core

--- transformato/restraints.py
+++ transformato/restraints.py
@@ -9,22 +9,25 @@ from simtk.openmm import *
 from simtk.openmm.app import *
 
 class Restraint():
-    def __init__(self,sel1,sel2,pdbpath,k=3,shape="harmonic"):
+    def __init__(self,selligand,selprotein,pdbpath,structure,k=3,shape="harmonic"):
         """Class representing a restraint to apply to the system
 
         Keywords:
-        g1,g2: An MDAnalysis selection string
+        selligand,selprotein: MDAnalysis selection strings
         k: the force (=spring) constant
+        pdbpath: the path to the pdbfile underlying the topology analysis
+        structure: structure1 or structure2 from the yaml
         shape: 'harmonic' or 'flat-bottom' (not yet implemented): Which potential to use for the energy function"""
 
         self.shape=shape
+        self.structure=structure
 
         if self.shape not in ["harmonic","flatbottom"]:
             raise AttributeError(f"Invalid potential shape specified for restraint: {self.shape}")
 
         self.topology=MDAnalysis.Universe(pdbpath)
-        self.g1=self.topology.select_atoms(sel1)
-        self.g2=self.topology.select_atoms(sel2)
+        self.g1=self.topology.select_atoms(selligand)
+        self.g2=self.topology.select_atoms(selprotein)
 
         self.g1pos=self.g1.center_of_mass()
         self.g2pos=self.g2.center_of_mass()
@@ -39,6 +42,22 @@ class Restraint():
         self.force_constant=k
         self.shape=shape
         
+        
+    def createForce(self,common_core_idxs):
+        """Actually creates the force, after dismissing all idxs not in the common core from the molecule
+        
+        Keywords:
+        common_core_idxs: Array - of the common core idxs"""
+
+        # Only done for g1, as it is the ligand group - theres no common core for the protein
+        self.g1_in_cc=[]
+        for id in self.g1_openmm:
+            if id in common_core_idxs:
+                self.g1_in_cc.append(id)
+            else:
+                print(f"Dismissing Atom IDX {id} as it is not in common core")
+        self.g2_in_cc=self.g2_openmm
+
         if self.shape=="harmonic":
             # create force with harmonic potential
             self.force=CustomCentroidBondForce(2,"0.5*k*(distance(g1,g2)-r0)^2")
@@ -54,8 +73,8 @@ class Restraint():
             raise NotImplementedError("Cant create flatbottom potential, as it has not yet been implemented")
 
         print(f"""Restraint force (centroid/bonded, shape is {self.shape}, initial distance: {self.initial_distance}) created:
-        Group 1 (COM: {self.g1pos}): {self.g1_openmm}
-        Group 2 (COM: {self.g2pos}): {self.g2_openmm}""")
+        Group 1 (COM: {self.g1pos}): {self.g1_in_cc}
+        Group 2 (COM: {self.g2pos}): {self.g2_in_cc}""")
     
     def get_force(self):
         return self.force
@@ -71,18 +90,18 @@ def CreateRestraintsFromConfig(configuration,pdbpath):
     restraints=[]
     if configuration["simulation"]["restraints"]=="auto":
         
-        restraints.append(Restraint(f"resname {tlc} and name C**" , f"(sphlayer 5 15 resname {tlc}) and name CA" , pdbpath))
+        restraints.append(Restraint(f"resname {tlc} and name C**" , f"(sphlayer 5 15 resname {tlc}) and name CA" , pdbpath,"structure1"))
     
     else:
         raise AttributeError(f"Error: demanded restraint type not supported :{configuration['simulation']['restraints']}")
 
-
+    return restraints
 
 
 
 # Testing when running as main script
 if __name__=="__main__":
-    print("Initialised as main program - runing unit tests")
+    print("Initialised as main program - runing unit tests on data/2OJ9")
     
 
     # generate a limited config for testing purposes
@@ -90,5 +109,6 @@ if __name__=="__main__":
     configuration["simulation"]["restraints"]="auto"
     configuration["system"]["structure1"]["tlc"]="BMI"
     configuration["system"]["structure2"]["tlc"]="UNK"
-    CreateRestraintsFromConfig(configuration,"../data/2OJ9-original/complex/openmm/step3_input.pdb")
-
+    restraintlist=CreateRestraintsFromConfig(configuration,"../data/2OJ9-original/complex/openmm/step3_input.pdb")
+    for restraint in restraintlist:
+        restraint.createForce([4824,4825,4826])
