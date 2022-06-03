b7f788d444d455678bd36b32da3b174bef1998bc
Alexander Grasser <a01526871@unet.univie.ac.at>
2022-06-03 23:17:58 +0200

    remove sidebar title

--- docs/conf.py
+++ docs/conf.py
@@ -107,7 +107,9 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 html_logo="assets/images/logo_transformato.png"
-
+html_theme_options={
+	"logo_only":True
+}
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
