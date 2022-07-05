3680ca3b84669c9e47bceaaeb52fa9ab359ab3be
JohannesKarwou <72743318+JohannesKarwou@users.noreply.github.com>
2022-07-05 09:54:51 +0200

    adding toppar_all36_moreions to the toppar.str

--- transformato/system.py
+++ transformato/system.py
@@ -189,6 +189,7 @@ class SystemStructure(object):
         parameter_files += (f"{toppar_dir}/par_all36_lipid.prm",)
         parameter_files += (f"{toppar_dir}/top_all36_lipid.rtf",)
         parameter_files += (f"{toppar_dir}/toppar_water_ions.str",)
+        parameter_files += (f"{toppar_dir}/toppar_all36_moreions.str",)
         parameter_files += (
             f"{toppar_dir}/toppar_all36_prot_na_combined.str",
         )  # if modified aminoacids are needed
