72fe6450a8a054e50c58f108fbc3b76cbefaefe8
wiederm <marcus.wieder@univie.ac.at>
2022-07-19 18:46:03 +0200

    remove unused impport, format with black

--- transformato/tests/test_restraints.py
+++ transformato/tests/test_restraints.py
@@ -10,7 +10,6 @@ Uses the following markers:
 
 Tests are mainly based on 2OJ9
 """
-from cgi import test
 import pytest
 import transformato.restraints as tfrs
 import transformato.utils as tfut
@@ -25,12 +24,14 @@ from simtk.unit import *
 from simtk.openmm import *
 from simtk.openmm.app import *
 
-TRAFO_DIR="./transformato/"
-PATH_2OJ9=f"{TRAFO_DIR}/../data/2OJ9-original/complex/openmm/step3_input.pdb"
-PATH_2OJ9_DIR=f"{TRAFO_DIR}/../data/2OJ9-original/complex/openmm/"
+TRAFO_DIR = "./transformato/"
+PATH_2OJ9 = f"{TRAFO_DIR}/../data/2OJ9-original/complex/openmm/step3_input.pdb"
+PATH_2OJ9_DIR = f"{TRAFO_DIR}/../data/2OJ9-original/complex/openmm/"
 
 
-sys.path.append(f"{TRAFO_DIR}/../data/2OJ9-original/complex/openmm/") # Enables module lookup in 2OJ9 for omm_ files
+sys.path.append(
+    f"{TRAFO_DIR}/../data/2OJ9-original/complex/openmm/"
+)  # Enables module lookup in 2OJ9 for omm_ files
 
 from omm_readinputs import *
 from omm_readparams import *
@@ -44,45 +45,58 @@ from pytest import approx
 
 # Disable useless parmed warnings for structure generation
 import warnings
-warnings.filterwarnings("ignore", module='parmed')
+
+warnings.filterwarnings("ignore", module="parmed")
 
 
 import logging
-logger=logging.getLogger(__name__)
+
+logger = logging.getLogger(__name__)
+
 
 @pytest.mark.restraints
 @pytest.mark.restraints_unittest
 def test_createRestraintsFromConfig():
-    
-    with open(f"{TRAFO_DIR}/tests/config/test-2oj9-restraints.yaml","r") as stream:
-        config=yaml.safe_load(stream)
-    
-    assert type(config)==dict # checks if config yaml is properly loaded
 
-    restraints=tfrs.create_restraints_from_config(config,PATH_2OJ9)
+    with open(f"{TRAFO_DIR}/tests/config/test-2oj9-restraints.yaml", "r") as stream:
+        config = yaml.safe_load(stream)
 
-    assert type(restraints)==list
+    assert type(config) == dict  # checks if config yaml is properly loaded
+
+    restraints = tfrs.create_restraints_from_config(config, PATH_2OJ9)
+
+    assert type(restraints) == list
 
     for restraint in restraints:
-        assert isinstance(restraint,tfrs.Restraint)
+        assert isinstance(restraint, tfrs.Restraint)
 
 
 @pytest.mark.restraints
 @pytest.mark.restraints_unittest
 def test_Restraints():
 
-    testrestraint=tfrs.Restraint("resname BMI and type C","protein and name CA",PATH_2OJ9,14)
+    testrestraint = tfrs.Restraint(
+        "resname BMI and type C", "protein and name CA", PATH_2OJ9, 14
+    )
 
-    testrestraint_fb=tfrs.Restraint("resname BMI and type C","protein and name CA",PATH_2OJ9,14,shape="flatbottom",wellsize=0.12)
+    testrestraint_fb = tfrs.Restraint(
+        "resname BMI and type C",
+        "protein and name CA",
+        PATH_2OJ9,
+        14,
+        shape="flatbottom",
+        wellsize=0.12,
+    )
     assert isinstance(testrestraint, tfrs.Restraint)
 
-    assert isinstance(testrestraint,tfrs.Restraint)
+    assert isinstance(testrestraint, tfrs.Restraint)
 
-    testrestraint.createForce(["C14","C12","C11","C9"]) # intentionally wrong CC
-    testrestraint_fb.createForce(["C14","C12","C11","C9"])
+    testrestraint.createForce(["C14", "C12", "C11", "C9"])  # intentionally wrong CC
+    testrestraint_fb.createForce(["C14", "C12", "C11", "C9"])
     print(testrestraint.g1_openmm)
-    assert [comp  in testrestraint.g1_openmm for comp in [4822,4825,4826,2828]] #Test proper selection and translation
-    
+    assert [
+        comp in testrestraint.g1_openmm for comp in [4822, 4825, 4826, 2828]
+    ]  # Test proper selection and translation
 
     assert isinstance(testrestraint.force, simtk.openmm.CustomCentroidBondForce)
 
@@ -90,35 +104,36 @@ def test_Restraints():
 
     assert isinstance(testrestraint_fb.force, simtk.openmm.CustomCentroidBondForce)
 
-    assert isinstance(testrestraint_fb.get_force(), simtk.openmm.CustomCentroidBondForce)
+    assert isinstance(
+        testrestraint_fb.get_force(), simtk.openmm.CustomCentroidBondForce
+    )
+
 
 @pytest.mark.restraints
 @pytest.mark.restraints_unittest
 def test_3DDistance():
-    assert(tfrs.get3DDistance(np.asarray([1,0,0]),np.asarray([0,0,0])))==1
+    assert (tfrs.get3DDistance(np.asarray([1, 0, 0]), np.asarray([0, 0, 0]))) == 1
+
 
 @pytest.mark.restraints
 @pytest.mark.restraints_unittest
 def test_write_yaml(tmp_path):
-
-    class MockSystem():
+    class MockSystem:
         def __init__(self):
-            self.tlc="LIG"
-            self.structure="structure2"
-    sys.modules["transformato.mutate"].cc_names_struc1=["C1","C2"]
-    sys.modules["transformato.mutate"].cc_names_struc2=["C1","C2"]
-    path=tmp_path/"test-config.yaml"
-    config=tfut.load_config_yaml(f"{TRAFO_DIR}/tests/config/test-2oj9-rsfe-restraints.yaml",".","./tmp/")
-    system=MockSystem()
-    current_step=4
-    tfrs.write_restraints_yaml(path,system,config,current_step)
+            self.tlc = "LIG"
+            self.structure = "structure2"
+
+    sys.modules["transformato.mutate"].cc_names_struc1 = ["C1", "C2"]
+    sys.modules["transformato.mutate"].cc_names_struc2 = ["C1", "C2"]
+    path = tmp_path / "test-config.yaml"
+    config = tfut.load_config_yaml(
+        f"{TRAFO_DIR}/tests/config/test-2oj9-rsfe-restraints.yaml", ".", "./tmp/"
+    )
+    system = MockSystem()
+    current_step = 4
+    tfrs.write_restraints_yaml(path, system, config, current_step)
 
     assert os.path.exists(path)
-
-
-
-
-
 
 
 @pytest.mark.restraints
@@ -130,16 +145,21 @@ def test_integration():
     Essentially a modified openmm_run.py
 
     """
-    
 
     inputs = read_inputs(f"{PATH_2OJ9_DIR}step5_production.inp")
-    params = CharmmParameterSet(*(glob.glob(f"{PATH_2OJ9_DIR}../*/*.prm")+glob.glob(f"{PATH_2OJ9_DIR}../*/*.str")
-    +glob.glob(f"{PATH_2OJ9_DIR}../*/*.rtf")+glob.glob(f"{PATH_2OJ9_DIR}../toppar/*.prm")))
+    params = CharmmParameterSet(
+        *(
+            glob.glob(f"{PATH_2OJ9_DIR}../*/*.prm")
+            + glob.glob(f"{PATH_2OJ9_DIR}../*/*.str")
+            + glob.glob(f"{PATH_2OJ9_DIR}../*/*.rtf")
+            + glob.glob(f"{PATH_2OJ9_DIR}../toppar/*.prm")
+        )
+    )
     psf = CharmmPsfFile(f"{PATH_2OJ9_DIR}step3_input.psf")
     crd = read_crd(f"{PATH_2OJ9_DIR}step3_input.crd")
 
-    top=gen_box(psf,crd)
-    
+    top = gen_box(psf, crd)
+
     # Build system
     nboptions = dict(
         nonbondedMethod=inputs.coulomb,
@@ -162,112 +182,127 @@ def test_integration():
     )
 
     # Set platform
-    
-    platform = Platform.getPlatformByName("CPU")
-    
-    prop = dict()
-    pdbpath=PATH_2OJ9
-    with open(f"{TRAFO_DIR}/tests/config/test-2oj9-restraints.yaml","r") as stream:
-        try:
-            configuration=yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-                print(exc)
 
-    cc_names=configuration["system"]["structure"]["ccs"]
+    platform = Platform.getPlatformByName("CPU")
+
+    prop = dict()
+    pdbpath = PATH_2OJ9
+    with open(f"{TRAFO_DIR}/tests/config/test-2oj9-restraints.yaml", "r") as stream:
+        try:
+            configuration = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    cc_names = configuration["system"]["structure"]["ccs"]
 
     # Add forces via transformato.restraints
-    
-    if not os.path.exists(pdbpath):
-        raise FileNotFoundError(f"Couldnt find {pdbpath} necessary for Restraint Analysis")
 
-    
-    restraintList=tfrs.create_restraints_from_config(configuration,pdbpath)
+    if not os.path.exists(pdbpath):
+        raise FileNotFoundError(
+            f"Couldnt find {pdbpath} necessary for Restraint Analysis"
+        )
+
+    restraintList = tfrs.create_restraints_from_config(configuration, pdbpath)
 
     # Test an additional, simple restraint
     logger.debug("generating simple selection")
-    selstr=tfrs.generate_simple_selection(configuration,pdbpath)
-    tlc=configuration["system"]["structure"]["tlc"]
-    restraintList.append(tfrs.Restraint(f"resname {tlc} and type C" , selstr , pdbpath))
+    selstr = tfrs.generate_simple_selection(configuration, pdbpath)
+    tlc = configuration["system"]["structure"]["tlc"]
+    restraintList.append(tfrs.Restraint(f"resname {tlc} and type C", selstr, pdbpath))
 
-    logger.debug("****************** ALL RESTRAINTS CREATED SUCCESSFULLY ***************************")
-    num_standard_forces=len(system.getForces())
+    logger.debug(
+        "****************** ALL RESTRAINTS CREATED SUCCESSFULLY ***************************"
+    )
+    num_standard_forces = len(system.getForces())
     for restraint in restraintList:
         restraint.createForce(cc_names)
     logger.debug("FORCES CREATED")
     for restraint in restraintList:
         restraint.applyForce(system)
     logger.debug("FORCES APPLIED")
-    for i,f in enumerate(system.getForces()):
+    for i, f in enumerate(system.getForces()):
         f.setForceGroup(i)
-    
-    forcesinsystem=system.getForces()
-    
-    
-    assert len(forcesinsystem)==num_standard_forces+len(restraintList)
+
+    forcesinsystem = system.getForces()
+
+    assert len(forcesinsystem) == num_standard_forces + len(restraintList)
 
     logger.debug(f"Number of forces in system: {len(forcesinsystem)}")
     simulation = Simulation(top.topology, system, integrator, platform, prop)
     simulation.context.setPositions(crd.positions)
 
-    
-    logger.info(f"Potential energy {simulation.context.getState(getEnergy=True).getPotentialEnergy()}")
+    logger.info(
+        f"Potential energy {simulation.context.getState(getEnergy=True).getPotentialEnergy()}"
+    )
 
-    
-    
-    simulation.minimizeEnergy(tolerance=inputs.mini_Tol*kilojoule/mole, maxIterations=10)
-    logger.info(f"Potential energy after 1-iter minimization {simulation.context.getState(getEnergy=True).getPotentialEnergy()}")
-    for i,f in enumerate(system.getForces()):
-        if isinstance(f,CustomCentroidBondForce):
-            state=simulation.context.getState(getEnergy=True,groups={i})
+    simulation.minimizeEnergy(
+        tolerance=inputs.mini_Tol * kilojoule / mole, maxIterations=10
+    )
+    logger.info(
+        f"Potential energy after 1-iter minimization {simulation.context.getState(getEnergy=True).getPotentialEnergy()}"
+    )
+    for i, f in enumerate(system.getForces()):
+        if isinstance(f, CustomCentroidBondForce):
+            state = simulation.context.getState(getEnergy=True, groups={i})
             logger.info("Force contributions before steps:")
             logger.info(f"{f}::{state.getPotentialEnergy()}")
     logger.info("Simulation stepping")
     simulation.step(9)
-    temp_results=[]
-    for i,f in enumerate(system.getForces()):
-        if isinstance(f,CustomCentroidBondForce):
-            state=simulation.context.getState(getEnergy=True,groups={i})
+    temp_results = []
+    for i, f in enumerate(system.getForces()):
+        if isinstance(f, CustomCentroidBondForce):
+            state = simulation.context.getState(getEnergy=True, groups={i})
             logger.info("Force contributions after 10 steps:")
             logger.info(f"{f}::{state.getPotentialEnergy()}")
             temp_results.append(state.getPotentialEnergy())
     simulation.step(10)
-    for i,f in enumerate(system.getForces()):
-        if isinstance(f,CustomCentroidBondForce):
-            state=simulation.context.getState(getEnergy=True,groups={i})
+    for i, f in enumerate(system.getForces()):
+        if isinstance(f, CustomCentroidBondForce):
+            state = simulation.context.getState(getEnergy=True, groups={i})
             logger.info("Force contributions after 20 steps:")
             logger.info(f"{f}::{state.getPotentialEnergy()}")
-    
-    assert all([results<0.1*kilojoule/mole for results in temp_results]) # assert all force contributions at this point are very small
-    assert 0*kilojoule/mole in temp_results # assert there is at least one flatbottom with 0 kJ/mol
+
+    assert all(
+        [results < 0.1 * kilojoule / mole for results in temp_results]
+    )  # assert all force contributions at this point are very small
+    assert (
+        0 * kilojoule / mole in temp_results
+    )  # assert there is at least one flatbottom with 0 kJ/mol
 
     # Change position of r0 and check force contributions now
     # Both harmonic and flatbottom potentials should be off the charts
-    
+
     logger.info("Changing Bond Parameters to ensure restraint activation")
 
-    for i,f in enumerate(system.getForces()):
-        if isinstance(f,CustomCentroidBondForce):
+    for i, f in enumerate(system.getForces()):
+        if isinstance(f, CustomCentroidBondForce):
             logger.debug(f.getBondParameters(0))
-            if f.getPerBondParameterName(0)=="k": # harmonic shape. openmmTools calls theirs "K"
-                f.setBondParameters(0,[0,1],[25,5])
-            else: # flatbottom shape:
-                f.setBondParameters(0,[0,1],[250,0.01])
+            if (
+                f.getPerBondParameterName(0) == "k"
+            ):  # harmonic shape. openmmTools calls theirs "K"
+                f.setBondParameters(0, [0, 1], [25, 5])
+            else:  # flatbottom shape:
+                f.setBondParameters(0, [0, 1], [250, 0.01])
             f.updateParametersInContext(simulation.context)
-    
-    for i,f in enumerate(system.getForces()):
-        if isinstance(f,CustomCentroidBondForce):
+
+    for i, f in enumerate(system.getForces()):
+        if isinstance(f, CustomCentroidBondForce):
             logger.debug(f.getBondParameters(0))
 
-    temp_results=[]
+    temp_results = []
     simulation.step(10)
-    for i,f in enumerate(system.getForces()):
-        if isinstance(f,CustomCentroidBondForce):
-            state=simulation.context.getState(getEnergy=True,groups={i})
+    for i, f in enumerate(system.getForces()):
+        if isinstance(f, CustomCentroidBondForce):
+            state = simulation.context.getState(getEnergy=True, groups={i})
             logger.info("Force contributions after 20 steps:")
             logger.info(f"{f}::{state.getPotentialEnergy()}")
             temp_results.append(state.getPotentialEnergy())
 
-    assert all([results>50*kilojoule/mole for results in temp_results]) # assert all force contributions at this point are rather large
-    assert all([results!=0*kilojoule/mole for results in temp_results]) # assert there is no restraint without force contribution
+    assert all(
+        [results > 50 * kilojoule / mole for results in temp_results]
+    )  # assert all force contributions at this point are rather large
+    assert all(
+        [results != 0 * kilojoule / mole for results in temp_results]
+    )  # assert there is no restraint without force contribution
 
     logger.info("Test complete")
