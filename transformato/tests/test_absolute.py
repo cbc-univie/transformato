0e64550731b747bff9bd2a8226b58c359322cf47
JohannesKarwou <johannes.karwou@gmail.com>
2022-07-20 16:31:54 +0200

    updating first test to run with GH

--- transformato/tests/test_absolute.py
+++ transformato/tests/test_absolute.py
@@ -1,3 +1,4 @@
+from venv import create
 from transformato import (
     load_config_yaml,
     SystemStructure,
@@ -9,32 +10,35 @@ import pytest
 import sys, os
 
 from transformato.annihilation import ProposeMutationRouteASFE
-from transformato.mutate import *
-from transformato.utils import *
+from transformato.mutate import perform_mutations
 from transformato.tests.paths import get_test_output_dir
+from transformato_testsystems.testsystems import get_testsystems_dir
 
 import warnings
 
 warnings.filterwarnings("ignore", module="parmed")
 
-@pytest.mark.rsfe
-@pytest.mark.skipif(
-    os.getenv("CI") == "true",
-    reason="Skipping tests that cannot pass in github actions",
-)
-def test_create_asfe_system():
 
-    configuration = load_config_yaml(
-        config="/site/raid2/johannes/transformato/transformato/tests/config/test-methanol-asfe.yaml",
-        input_dir="data/",
-        output_dir="/site/raid4/johannes/test-RSFE/run_4/",
-    )
+def create_asfe_system(configuration):
 
     s1 = SystemStructure(configuration, "structure1")
     s1_absolute = ProposeMutationRouteASFE(s1)
     s1_absolute.propose_common_core()
     s1_absolute.finish_common_core()
     mutation_list = s1_absolute.generate_mutations_to_common_core_for_mol1()
+
+    return s1, mutation_list
+
+
+def test_create_asfe_system():
+
+    configuration = load_config_yaml(
+        config=f"{get_testsystems_dir()}/config/methanol-asfe.yaml",
+        input_dir=get_testsystems_dir(),
+        output_dir=get_test_output_dir(),
+    )
+
+    s1, mutation_list = create_asfe_system(configuration)
 
     i = IntermediateStateFactory(system=s1, configuration=configuration)
 
@@ -44,6 +48,9 @@ def test_create_asfe_system():
         i=i,
         mutation_list=mutation_list,
     )
+
+    assert len(i.output_files) == 7
+    assert len((mutation_list)["charge"][0].atoms_to_be_mutated) == 6
 
     # ddG_openMM, dddG, f_openMM = postprocessing(
     #     configuration,
