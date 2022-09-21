789d68bd6775e774db9a20ff95ed43b8f1560865
JohannesKarwou <johannes.karwou@gmail.com>
2022-09-21 21:35:28 +0200

    adapting submit skript

--- transformato/bin/simulation-rsfe_charmm.sh
+++ transformato/bin/simulation-rsfe_charmm.sh
@@ -11,7 +11,7 @@ hostname
 
 
 input=charmm_run_vacuum
-OMM_NUM_THREADS=8 charmm_c47_omm_domdecgpu -i ${input}.inp > log_vac.out
+OMP_NUM_THREADS=8 charmm_c47_omm_domdecgpu -i ${input}.inp > log_vac.out
 
 input=charmm_run_waterbox
-OMM_NUM_THREADS=8 charmm_c47_omm_domdecgpu -i ${input}.inp > log_solv.out
+OMP_NUM_THREADS=8 charmm_c47_omm_domdecgpu -i ${input}.inp > log_solv.out
