73c860578daf1731c3effbdd2f14231f13824fee
loeflerm <marvin.loefler@gmail.com>
2024-05-21 20:09:33 +0200

    unl.str trouble #2

--- transformato/__init__.py
+++ transformato/__init__.py
@@ -33,3 +33,6 @@ __version__ = _version.get_versions()["version"]
 from . import _version
 
 __version__ = _version.get_versions()["version"]
+
+from . import _version
+__version__ = _version.get_versions()['version']
