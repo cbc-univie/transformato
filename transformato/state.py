360110244be5a68b1f383c907ebe637599fc341e
loeflerm <marvin.loefler@gmail.com>
2024-08-22 12:04:00 +0200

    another filetest

--- transformato/state.py
+++ transformato/state.py
@@ -586,10 +586,10 @@ class IntermediateStateFactory(object):
                         g.write(line)
                     elif i == 1:
                         i += 1
-                        g.write("platform = Platform.getPlatformByName('OpenCL')\n")
+                        g.write("platform = Platform.getPlatformByName('OpenCLeeeee')\n")
                     elif i == 2:
                         i += 2
-                        g.write("prop = dict(UseCpuPme='true')\n")
+                        g.write("prop = dict(UseCpuPme='trueeeeeeeee')\n")
                     else:
                         g.write(line)
             elif self.configuration["simulation"]["GPU"].upper() == "CUDA":
